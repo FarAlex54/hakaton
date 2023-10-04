8044341ac9490dadc082bc4a54ed6983ef08a943
FarAlex54 <sibacadem@mail.ru>
2023-10-04 13:40:32 +0700

    NewChanges

--- src/App.jsx
+++ src/App.jsx
@@ -1,84 +1,72 @@
-import './App.css';
-import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import MapPos from './MapPos';
-import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
-import { useState, useEffect } from "react";
-
-
-
 /* Широта: 55.0095388, Долгота: 82.4246794, Точность: 20 м */
 /* Широта: 54.9832693, Долгота: 82.8963831, Точность: 12053.58603605653 м */
 /*   "homepage": "https://github.com/FarAlex54/hakaton.git", */
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const APIkey = "301284d6b96c42f8825c6077114ba012";
 
 function App() {
-  const [lat, setLat] = useState();
-  const [long,setLong] = useState();
-  const [accuracy,setAccuracy] = useState();
+  const [location, setLocation] = useState();
+
+  function getLocationInfo(latitude, longitude) {
+    const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&key=${APIkey}`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.status.code === 200) {
+          console.log("results:", data.results);
+          setLocation(data.results[0].formatted);
+        } else {
+          console.log("Reverse geolocation request failed.");
+        }
+      })
+      .catch((error) => console.error(error));
+  }
   var options = {
     enableHighAccuracy: true,
     timeout: 5000,
-    maximumAge: 10000
+    maximumAge: 0,
   };
+  function success(pos) {
+    var crd = pos.coords;
+    console.log("Your current position is:");
+    console.log(`Latitude : ${crd.latitude}`);
+    console.log(`Longitude: ${crd.longitude}`);
+    console.log(`More or less ${crd.accuracy} meters.`);
 
-  function errors() {
-    console.log('Произошла ошибка');
+    getLocationInfo(crd.latitude, crd.longitude);
+  }
+
+  function errors(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
   }
 
   useEffect(() => {
-    const locateUser = () => {
     if (navigator.geolocation) {
       navigator.permissions
         .query({ name: "geolocation" })
-        .then((result) => {
-                  if (result.state === "granted") {
-                    navigator.geolocation.getCurrentPosition((pos)=>{setLat(pos.coords.latitude);
-                                                                     setLong(pos.coords.longitude);
-                                                                     setAccuracy(pos.coords.accuracy);}, errors, options);
-                  }
-                });}}
-    locateUser();
-  },[]);
- 
+        .then(function (result) {
+          console.log(result);
+          if (result.state === "granted") {
+            //If granted then you can directly call your function here
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "prompt") {
+            //If prompt then the user will be asked to give permission
+            navigator.geolocation.getCurrentPosition(success, errors, options);
+          } else if (result.state === "denied") {
+            //If denied then you have to show instructions to enable location
+          }
+        });
+    } else {
+      console.log("Geolocation is not supported by this browser.");
+    }
+  }, []);
+
   return (
     <div className="App">
-      <Form>
-        <div className="d-flex flex-row">
-          <div className='p-2 col-9 bg-danger'>
-            <p>Широта:{lat}</p>
-            <p>Долгота:{long}</p>
-            <p>Точность:{accuracy}</p>
-            <p>Точность:{accuracy}</p>
-{/*             <YMaps>vcdvdsv
-                <Map defaultState={{ center: [lat, long], zoom: 15 }}>
-                  <Placemark 
-                  modules={["geoObject.addon.balloon"]}
-                  defaultGeometry={[lat, long]}
-                  properties={{
-                    balloonContentBody:
-                      "Широта: " + lat + ", Долгота: " + long + ", Точность: " + accuracy + " м",
-                  }}/>
-                </Map>
-            </YMaps> */}
-          </div>
-            {console.log("Широта: " + lat + ", Долгота: " + long + ", Точность: " + accuracy + " м")}
-          <div className='p-2 col-3'>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="БанкУслуга 1" />
-                <Form.Check type="checkbox" label="БанкУслуга 2" />
-                <Form.Check type="checkbox" label="БанкУслуга 3" />
-                <Form.Check type="checkbox" label="БанкУслуга 4" />
-                <Form.Check type="checkbox" label="БанкУслуга 5" />
-                <Form.Check type="checkbox" label="БанкУслуга 6" />
-                <Form.Check type="checkbox" label="БанкУслуга 7" />
-                <Form.Check type="checkbox" label="БанкУслуга 8" />
-              </Form.Group>
-              <Button variant="success">Поиск</Button>{' '}
-          </div>
-        </div>
-      </Form>
+      {location ? <>Your location: {location}</> : null}
     </div>
   );
 }
